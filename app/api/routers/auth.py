bf8ef4d01783b172f0f17cdf0c018ccee1bafc8e
Dilyaver <yxs.crevier@gmail.com>
2025-10-28 10:25:18 +0500

    fastapi-users access token 1 week lifetime

--- app/api/routers/auth.py
+++ app/api/routers/auth.py
@@ -19,32 +19,24 @@ router = APIRouter()
 
 router.include_router(
     fastapi_users.get_auth_router(auth_backend, requires_verification=False), # TODO: change to True in production
-    prefix="/auth/cookie", # Изменено с /auth/jwt
+    prefix="/auth/cookie",
     tags=["auth"],
 )
-
-
 router.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix="/auth",
     tags=["auth"],
 )
-
-
 router.include_router(
     fastapi_users.get_verify_router(UserRead),
     prefix="/auth",
     tags=["auth"],
 )
-
-
 router.include_router(
     fastapi_users.get_reset_password_router(),
     prefix="/auth",
     tags=["auth"],
 )
-
-
 router.include_router(
     fastapi_users.get_users_router(UserRead, UserUpdate),
     prefix="/users",
